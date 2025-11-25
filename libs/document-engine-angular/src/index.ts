715addd5f20683e969dde564ef23a72ddccff317
Phuong Tran <phuong.tran@realestatedoc.co>
2025-11-25 09:36:30 +0700

    fix: bump version to 0.0.23 for document-engine-angular and document-engine-core

--- libs/document-engine-angular/src/index.ts
+++ libs/document-engine-angular/src/index.ts
@@ -4,3 +4,6 @@ export * from './lib/ui';
 export * from './lib/types';
 export * from './lib/utils';
 export * from './lib/views';
+
+// Re-export types from core for consumer convenience
+export type { Editor } from '@tiptap/core';
--- libs/document-engine-angular/src/lib/components/index.ts
+++ libs/document-engine-angular/src/lib/components/index.ts
@@ -1,5 +1,2 @@
 export * from './document-editor.component';
 export * from './document-editor.module';
-
-// Re-export types from core for consumer convenience
-export type { Editor } from '@tiptap/core';
